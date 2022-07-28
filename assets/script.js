01275608c749b736b25d973b9c0f247d03056a08
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-28 19:43:17 -0400

    Adding note and save variables

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,15 @@
 var currentDayEl = $('#currentDay');
 var currentTimeEL = moment().format('LL');
+var task = document.querySelector('.entry');
+var saveButton = document.querySelector('.saveBtn');
+var note = document.getElementById("note");
+var save = document.getElementById("save");
 
 function displayTime() {
     var rightNow = moment().format('LLLL');
     currentDayEl.text(rightNow);
 }
+
+
+
 (setInterval(displayTime, 1000));
