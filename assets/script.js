dd60d5d14b83e962b624c834013b46c57990d353
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-28 22:03:47 -0400

    Adding time slot variables

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,17 @@ var saveButton = document.querySelector('.saveBtn');
 var note = document.getElementById("note");
 var save = document.getElementById("save");
 
+var nineOclock = document.getElementById("hour-1");
+var tenOclock = document.getElementById("hour-2");
+var elevenOclock = document.getElementById("hour-3");
+var twelveOclock = document.getElementById("hour-4");
+var oneOclock = document.getElementById("hour-5");
+var twoOclock = document.getElementById("hour-6");
+var threeOclock = document.getElementById("hour-7");
+var fourOclock = document.getElementById("hour-8");
+var fiveOclock = document.getElementById("hour-9");
+var timeBlocks = [nineOclock, tenOclock, elevenOclock, twelveOclock, oneOclock, twoOclock, threeOclock, fourOclock, fiveOclock];
+
 function displayTime() {
     var rightNow = moment().format('LLLL');
     currentDayEl.text(rightNow);
